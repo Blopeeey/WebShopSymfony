4395fdd5db6cc3b2d843df42ad4ee10b688a5222
Blopeeey <basweidmann@hotmail.com>
2019-09-18 21:20:17 +0200

    '-'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\OrderProduct;
 use App\Entity\Product;
+use App\Entity\User;
 use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -42,6 +45,41 @@ class DefaultController extends AbstractController
 
         return $this->render('default/viewproduct.html.twig', [
             'Products' => $prod
+        ]);
+    }
+
+    /**
+     * @Route("/checkout", name="checkout"])
+     */
+    public function checkout()
+    {
+        $session = $this->session->get('cart');
+        $cartarray = [];
+        $totaal = 0;
+        foreach($session as $yee => $product){
+            $res = $this->getDoctrine()->getRepository(Product::class)->find($yee);
+            array_push($cartarray, [$yee, $product['aantal'], $res]);
+            $totaal = $totaal + ($product['aantal'] * $res->getPrice());
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $order = new Order();
+        $order->setUser($this->getUser());
+        $em->persist($order);
+
+        $orderprod = new OrderProduct();
+        $orderprod->setProduct($res);
+        $orderprod->setOrderr($order);
+        $orderprod->setAantal($product['aantal']);
+        $em->persist($orderprod);
+
+        $em->flush();
+
+        $this->session->clear();
+
+        return $this->render('default/index.html.twig', [
+
         ]);
     }
 
